47da09332e162d627ce404e88a694862b076ffba
wiley68 <ilko.iv@gmail.com>
2020-07-09 07:20:36 +0300

    print table product

--- resources/views/orders/print.blade.php
+++ resources/views/orders/print.blade.php
@@ -94,7 +94,22 @@
                         }
             		break;
             		case 't':
-		                $product_me_txt = 'изделие';
+                        $product_me_txt = 'изделие';
+                        if (($suborder->h !== '') && ($suborder->h !== 0)){
+                            $visocina = "Височина: " . $suborder->h . " mm ";
+                        }else{
+                            $visocina = "";
+                        }
+                        if (($suborder->l !== '') && ($suborder->l !== 0)){
+                            $shirina = "Ширина: " . $suborder->l . " mm ";
+                        }else{
+                            $shirina = "";
+                        }
+                        if (($suborder->p !== '') && ($suborder->p !== 0)){
+                            $dalbocina = "Дълбочина: " . $suborder->p . " mm";
+                        }else{
+                            $dalbocina = "";
+                        }
             		break;
 		            default:
 		                $product_me_txt = 'брой';
