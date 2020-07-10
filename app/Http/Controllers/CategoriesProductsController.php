ab199334e2baf80f5aa29369cdc3c1bf10c8592c
wiley68 <ilko.iv@gmail.com>
2020-07-10 10:50:47 +0300

    category shop

--- app/Http/Controllers/CategoriesProductsController.php
+++ app/Http/Controllers/CategoriesProductsController.php
@@ -12,7 +12,16 @@ class CategoriesProductsController extends Controller
     }
 
     public static function getCategories(){
-        $categories = CategoriesProducts::all();
+        $categories = CategoriesProducts::where('isshop', 1)->get();
         return $categories;
+    }
+
+    public static function getCategoriesByShop(){
+        $categories = CategoriesProducts::select('id')->where('isshop', 1)->get();
+        $categories_arr = [];
+        foreach ($categories as $category) {
+            $categories_arr[] = $category->id;
+        }
+        return $categories_arr;
     }
 }
--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,24 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\Http\Controllers\FirmsController;
+use App\Http\Controllers\CategoriesProductsController;
 
 class ProductsController extends Controller
 {
     public static function getAllProducts(){
-        $firms_id = FirmsController::getFirmsByIsshop();
-		$products = Product::whereIn('firm_id', $firms_id)->where('isshop', '>', 0)->get();
+		$firms_id = FirmsController::getFirmsByIsshop();
+		$category_id = CategoriesProductsController::getCategoriesByShop();
+		$products = Product::whereIn('firm_id', $firms_id)->whereIn('category_id', $category_id)->where('isshop', '>', 0)->get();
 		return $products;
 	}
 
     public static function getProducts(){
-        $firms_id = FirmsController::getFirmsByIsshop();
-		$products = Product::whereIn('firm_id', $firms_id)->where('isshop', '>', 0)->orderBy('name')->paginate(9);
+		$firms_id = FirmsController::getFirmsByIsshop();
+		$category_id = CategoriesProductsController::getCategoriesByShop();
+		$products = Product::whereIn('firm_id', $firms_id)->whereIn('category_id', $category_id)->where('isshop', '>', 0)->orderBy('name')->paginate(9);
 		return $products;
 	}
 
 	public static function getProductsByFirm($firm_id){
-        $firms_id = FirmsController::getFirmsByIsshop();
-		$products = Product::whereIn('firm_id', $firms_id)->where('firm_id', $firm_id)->where('isshop', '>', 0)->paginate(9);
+		$firms_id = FirmsController::getFirmsByIsshop();
+		$category_id = CategoriesProductsController::getCategoriesByShop();
+		$products = Product::whereIn('firm_id', $firms_id)->whereIn('category_id', $category_id)->where('firm_id', $firm_id)->where('isshop', '>', 0)->paginate(9);
 		return $products;
     }
     
@@ -32,7 +36,8 @@ class ProductsController extends Controller
 
 	public static function getProductsByTermin($termin){
 		$firms_id = FirmsController::getFirmsByIsshop();
-		$products = Product::whereIn('firm_id', $firms_id)
+		$category_id = CategoriesProductsController::getCategoriesByShop();
+		$products = Product::whereIn('firm_id', $firms_id)->whereIn('category_id', $category_id)
 			->where('name', 'like', '%'.$termin.'%')
 			->orWhere('code', 'like', '%'.$termin.'%')
 			->where('isshop', '>', 0)
@@ -41,27 +46,30 @@ class ProductsController extends Controller
 	}
 
 	public static function getProductsByTerminId($termin, $id){
+		$category_id = CategoriesProductsController::getCategoriesByShop();
 		$products = Product::where([['name', 'like', '%'.$termin.'%'],['firm_id','=', $id]])
 			->orWhere([['code','like', '%'.$termin.'%'],['firm_id','=', $id]])
-			->where('isshop', '>', 0)
+			->where('isshop', '>', 0)->whereIn('category_id', $category_id)
 			->paginate(9);
 
 		return $products;
 	}
 
 	public static function getProductsRandom($nomber){
-        $firms_id = FirmsController::getFirmsByIsshop();
-		$product_count = Product::whereIn('firm_id', $firms_id)->where('isshop', '>', 0)->count();
+		$firms_id = FirmsController::getFirmsByIsshop();
+		$category_id = CategoriesProductsController::getCategoriesByShop();
+		$product_count = Product::whereIn('firm_id', $firms_id)->whereIn('category_id', $category_id)->where('isshop', '>', 0)->count();
 		if ($product_count > $nomber){
 			$product_count = $nomber;
 		}
-		$products = Product::whereIn('firm_id', $firms_id)->where('isshop', '>', 0)->get()->random($product_count);
+		$products = Product::whereIn('firm_id', $firms_id)->whereIn('category_id', $category_id)->where('isshop', '>', 0)->get()->random($product_count);
 		return $products;
 	}
 
 	public static function getProductsByFirmRandom($firm_id, $code, $nomber){
-        $firms_id = FirmsController::getFirmsByIsshop();
-		$products = Product::whereIn('firm_id', $firms_id)->where([['firm_id', '=', $firm_id],['code', '<>', $code],['isshop', '>', 0],])->inRandomOrder()->limit($nomber)->get();
+		$firms_id = FirmsController::getFirmsByIsshop();
+		$category_id = CategoriesProductsController::getCategoriesByShop();
+		$products = Product::whereIn('firm_id', $firms_id)->whereIn('category_id', $category_id)->where([['firm_id', '=', $firm_id],['code', '<>', $code],['isshop', '>', 0],])->inRandomOrder()->limit($nomber)->get();
 		return $products;
 	}
 
